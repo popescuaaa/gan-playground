be1bbc71e59e7ed30c95b8c63855e1e94b51432b
Popescu Andrei <andreigabrielpopescu556@gmail.com>
2020-12-11 17:52:58 +0200

    Added sample and get_visual_sample for TGenerator

--- time_gan_2017/TGenerator.py
+++ time_gan_2017/TGenerator.py
@@ -1,33 +1,50 @@
 import torch
 import torch.nn as nn
 
+"""
+    An LSTM based generator. It expects a sequence of noise vectors as input.
+"""
+
 
 class LSTMGenerator(nn.Module):
-    """An LSTM based generator. It expects a sequence of noise vectors as input.
-    Args:
-        in_dim: Input noise dimensionality
-        out_dim: Output dimensionality
-        n_layers: number of lstm layers
-        hidden_dim: dimensionality of the hidden layer of lstms
-    Input: noise of shape (batch_size, seq_len, in_dim)
-    Output: sequence of shape (batch_size, seq_len, out_dim)
-    """
 
-    def __init__(self, in_dim, out_dim, n_layers=1, hidden_dim=256):
+    def __init__(self, dim_latent, dim_output, dist_latent, layers_num=1, dim_hidden=256):
         super().__init__()
-        self.n_layers = n_layers
-        self.hidden_dim = hidden_dim
-        self.out_dim = out_dim
+        self.n_layers = layers_num
+        self.hidden_dim = dim_hidden
+        self.out_dim = dim_output
+        self.dist_latent = dist_latent
 
-        self.lstm = nn.LSTM(in_dim, hidden_dim, n_layers, batch_first=True)
-        self.linear = nn.Sequential(nn.Linear(hidden_dim, out_dim), nn.Tanh())
+        # x = noise from dim latent
+        # lstm(
+        #       dim_x,
+        #      hidden state features dimension,
+        #      how many lstm cells
+        #      ) =>out and a tuple (hn, cn) => the nth output pairs (important order)
+        # out = tensor containing the output features (h_t) from the last layer of the LSTM
+        # we take the out value => linear and tanh => [-1, 1]
 
-    def forward(self, input):
-        batch_size, seq_len = input.size(0), input.size(1)
+        self.lstm = nn.LSTM(dim_latent, dim_hidden, layers_num, batch_first=True)
+        self.linear = nn.Sequential(nn.Linear(dim_hidden, dim_output), nn.Tanh())
+
+    def forward(self, x):
+        batch_size, seq_len = x.size(0), x.size(1)
+
+        # initial values
+
         h_0 = torch.zeros(self.n_layers, batch_size, self.hidden_dim)
         c_0 = torch.zeros(self.n_layers, batch_size, self.hidden_dim)
 
-        recurrent_features, _ = self.lstm(input, (h_0, c_0))
+        recurrent_features, _ = self.lstm(x, (h_0, c_0))
         outputs = self.linear(recurrent_features.contiguous().view(batch_size * seq_len, self.hidden_dim))
         outputs = outputs.view(batch_size, seq_len, self.out_dim)
         return outputs
+
+    def sample(self, num_samples) -> torch.Tensor:
+        z = self.dist_latent.sample(sample_shape=(num_samples, self.dim_latent))
+        return z
+
+    def generate_visual_sample(self, num_samples):
+        z = self.dist_latent.sample(num_samples)
+        x = self.forward(z)
+        return x
