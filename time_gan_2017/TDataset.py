b28176540bc3f01d1068363d196777227a2016c2
Popescu Andrei <andreigabrielpopescu556@gmail.com>
2021-01-06 14:20:49 +0200

    Added tensor like conversion for dataset

--- time_gan_2017/TDataset.py
+++ time_gan_2017/TDataset.py
@@ -1,23 +1,37 @@
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
-
+import torch
 import numpy as np
 
 DATASET_CONFIG = ['Open' 'High' 'Low' 'Close' 'Adj_Close' 'Volume']
 
 
 class StockDataset(Dataset):
-    def __init__(self, normalize=False):
+    def __init__(self, seq_len=10, normalize=False):
+        # sequence len is limited by numpy at 32
+
         self.row_data = np.loadtxt('./stock_data.csv', delimiter=',', dtype=np.str)
+
         self.data = [float(e[3]) for e in self.row_data[1:]]
-        self.len = len(self.data)
-        self.max = max(self.data)
+        tensor_like = []
+
+        for idx in range(0, len(self.data) // seq_len, seq_len):
+            tensor_like.append(self.data[idx: idx + seq_len])
+        print(len(tensor_like))
+
+        self.data = torch.FloatTensor(tensor_like)
+
+        self.len = self.data.shape[1]
+        self.max = self.data.max()
+
+        # There is the same asset price but they can be grouped in small chunks
+        # of length seq_len as they will irl by date
 
         if normalize:
             self.data = self.normalize()
 
     def normalize(self):
-        normalized_data = [float(e / self.max) for e in self.data]
+        normalized_data = self.data / self.max
         return normalized_data
 
     def denormalize(self):
@@ -35,8 +49,9 @@ if __name__ == '__main__':
     # dataset test
     ds = StockDataset()
     dl = DataLoader(ds, batch_size=10, shuffle=True, num_workers=10)
-    for i, batch in enumerate(dl):
-        print('Index: {} data {}'.format(i, batch))
+    for i, real in enumerate(dl):
+        print('Index: {} data {}'.format(i, real))
+        print(real.shape)
         break
 
     ds = StockDataset(normalize=True)
