73d061df934811f65d92b11b2e4ed65b9b39980c
Popescu Andrei <andreigabrielpopescu556@gmail.com>
2021-01-28 19:46:00 +0200

    Removed contained dist latent to silpify the model

--- time_gan_2017/TGenerator.py
+++ time_gan_2017/TGenerator.py
@@ -8,49 +8,31 @@ import torch.nn as nn
 
 class LSTMGenerator(nn.Module):
 
-    def __init__(self, dim_latent, dim_output, dist_latent, num_layers=1, dim_hidden=256):
+    def __init__(self, dim_latent, dim_output, num_layers=1, dim_hidden=256):
         super().__init__()
         self.num_layers = num_layers
         self.dim_hidden = dim_hidden
         self.dim_output = dim_output
-        self.dist_latent = dist_latent
         self.dim_latent = dim_latent
 
-        self.seq_len = None
-        self.batch_size = None
-
-        # x = noise from dim latent
-        # lstm(
-        #       dim_x,
-        #      hidden state features dimension,
-        #      how many lstm cells
-        #      ) =>out and a tuple (hn, cn) => the nth output pairs (important order)
-        # out = tensor containing the output features (h_t) from the last layer of the LSTM
-        # we take the out value => linear and tanh => [-1, 1]
-
-        self.lstm = nn.LSTM(dim_latent, self.dim_hidden,  self.num_layers, batch_first=True)
+        '''
+         x = noise from dim latent
+        lstm(
+             dim_x,
+             hidden state features dimension,
+             how many lstm cells
+             ) =>out and a tuple (hn, cn) => the nth output pairs (important order)
+             
+        out = tensor containing the output features (h_t) from the last layer of the LSTM
+        '''
+        self.lstm = nn.LSTM(self.dim_latent, self.dim_hidden, self.num_layers, batch_first=True)
         self.linear = nn.Sequential(nn.Linear(self.dim_hidden, self.dim_output))
 
-        # tanh => increase with a value form [-1, 1]
-
     def forward(self, x):
-        self.batch_size, self.seq_len = x.size(0), x.size(1)
-
-        # initial values
-        # linear for h_0, c_0 ->
-        h_0 = torch.zeros(self.num_layers, self.batch_size, self.dim_hidden).cuda()
-        c_0 = torch.zeros(self.num_layers, self.batch_size, self.dim_hidden).cuda()
-
+        batch_size, seq_len = x.size(0), x.size(1)
+        h_0 = torch.zeros(self.num_layers, batch_size, self.dim_hidden).cuda()
+        c_0 = torch.zeros(self.num_layers, batch_size, self.dim_hidden).cuda()
         recurrent_features, _ = self.lstm(x, (h_0, c_0))
-        outputs = self.linear(recurrent_features.contiguous().view(self.batch_size * self.seq_len, -1))
-        outputs = outputs.view(self.batch_size, self.seq_len, self.dim_output)
+        outputs = self.linear(recurrent_features.contiguous().view(batch_size * seq_len, self.dim_hidden))
+        outputs = outputs.view(batch_size, seq_len, self.dim_output)
         return outputs
-
-    def sample(self, num_samples, batch_size) -> torch.Tensor:
-        z = self.dist_latent.sample(sample_shape=(num_samples * batch_size, self.dim_latent))
-        return z
-
-    def generate_visual_sample(self, num_samples, batch_size):
-        z = self.sample(num_samples, batch_size).view(self.seq_len, batch_size, self.dim_latent).cuda()
-        x = self.forward(z)
-        return x
