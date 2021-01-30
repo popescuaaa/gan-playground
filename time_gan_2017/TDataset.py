a6a6cc3b8f93047e9232128363b169eadc29aeed
Popescu Andrei <andreigabrielpopescu556@gmail.com>
2021-01-30 13:53:27 +0200

    Added custom version that returns a sequence of deltas and start value

--- time_gan_2017/TDataset.py
+++ time_gan_2017/TDataset.py
@@ -41,13 +41,46 @@ class StockDataset(Dataset):
         return self.data[idx]
 
 
-if __name__ == '__main__':
-    # dataset test
+class StockDatasetDeltas(Dataset):
+    """
+    Dataset return a sequence: [ start_value : sequence_deltas ]
+    Actual sequence len: seq_len - 1
+    """
+
+    def __init__(self, seq_len=10):
+        # sequence len is limited by numpy at 32
+
+        self.row_data = np.loadtxt('./stock_data.csv', delimiter=',', dtype=np.str)
+
+        self.data = [float(e[3]) for e in self.row_data[1:]]
+        tensor_like = []
+
+        for idx in range(0, len(self.data) - seq_len):
+            deltas = np.subtract(np.array(self.data[idx: idx + seq_len][1:]),
+                                 np.array(self.data[idx: idx + seq_len][:-1]))
+
+            deltas = np.concatenate([np.array([self.data[idx: idx + seq_len][0]]),
+                                     deltas])
+
+            tensor_like.append(deltas)
+
+        self.data = torch.FloatTensor(tensor_like)
+        self.len = self.data.shape[0]
+        self.mean = self.data.mean()
+        self.std = torch.std(self.data)
+
+    def __len__(self):
+        return self.len
+
+    def __getitem__(self, idx):
+        return self.data[idx]
+
+
+def test_stock_dataset() -> None:
     ds = StockDataset()
     dl = DataLoader(ds, batch_size=5, shuffle=True, num_workers=10)
     for i, real in enumerate(dl):
         print('Index: {} data {}'.format(i, real))
-        print(real.shape)
         break
 
     ds = StockDataset(normalize=True)
@@ -55,3 +88,15 @@ if __name__ == '__main__':
     for i, batch in enumerate(dl):
         print('Index: {} data {}'.format(i, batch))
         break
+
+
+def test_deltas_stock_dataset() -> None:
+    ds = StockDatasetDeltas()
+    dl = DataLoader(ds, batch_size=5, shuffle=False, num_workers=10)
+    for i, real in enumerate(dl):
+        print('Index: {} data {}'.format(i, real))
+        break
+
+
+if __name__ == '__main__':
+    test_deltas_stock_dataset()
