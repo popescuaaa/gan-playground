216842ba34780a7eb0192d97c71af40c2ea2f656
Popescu Andrei <andreigabrielpopescu556@gmail.com>
2021-02-23 18:43:31 +0200

    Refactor old stock dataset

--- time_gan_2017/TDataset.py
+++ time_gan_2017/TDataset.py
@@ -5,104 +5,42 @@ import numpy as np
 import pandas as pd
 from random import choice  # for dt sampling
 
-DATASET_CONFIG = ['Open' 'High' 'Low' 'Close' 'Adj_Close' 'Volume']
+DATASET_CONFIG = ['Open', 'High', 'Low', 'Close']  # for test purpose
 
 
 class StockDataset(Dataset):
-    def __init__(self, seq_len=10, normalize=False):
-        # sequence len is limited by numpy at 32
+    def __init__(self, csv_path: str, seq_len: int, config: str):
+        assert (config in DATASET_CONFIG), 'Config element is not supported'
+        self.seq_len = seq_len
+        self.df = pd.read_csv(csv_path)
 
-        self.row_data = np.loadtxt('./stock_data.csv', delimiter=',', dtype=np.str)
+        # Compute ∆t (deltas)
+        self.dt = np.array([(self.df[config][i + 1] - self.df[config][i])
+                            for i in range(self.df[config].size - 1)])
+        self.dt = np.concatenate([np.array([0]), self.dt])
 
-        self.data = [float(e[3]) for e in self.row_data[1:]]
-        tensor_like = []
+        # Append ∆t (deltas)
+        self.df.insert(DATASET_CONFIG.index(config) + 1, 'dt', self.dt)
 
-        for idx in range(0, len(self.data) - seq_len):
-            tensor_like.append(self.data[idx: idx + seq_len])
+        # Create two structures for data and ∆t
+        self.stock_data = [torch.from_numpy(np.array(self.df[config][i: i + self.seq_len]))
+                           for i in range(self.df.size - self.seq_len - 1)]
 
-        self.data = torch.FloatTensor(tensor_like)
-        self.len = self.data.shape[0]
-        self.mean = self.data.mean()
-        self.std = torch.std(self.data)
+        self.dt_data = [torch.from_numpy(np.array(self.df.dt[i: i + self.seq_len]))
+                        for i in range(self.df.size - self.seq_len - 1)]
 
-        if normalize:
-            self.data = self.normalize()
+        # Filter for small size chunks
+        self.stock_data = list(filter(lambda t: t.shape[0] == seq_len, self.stock_data))
+        self.dt_data = list(filter(lambda t: t.shape[0] == seq_len, self.dt_data))
 
-    def normalize(self):
-        normalized_data = (self.data - self.mean) / self.std
-        return normalized_data
-
-    def denormalize(self):
-        denormalized_data = self.data * self.std + self.mean
-        return denormalized_data
-
-    def __len__(self):
-        return self.len
-
-    def __getitem__(self, idx):
-        return self.data[idx]
-
-
-class StockDatasetDeltas(Dataset):
-    """
-    Dataset return a sequence: [ start_value : sequence_deltas ]
-    Actual sequence len: seq_len - 1
-    """
-
-    def __init__(self, seq_len=10):
-        # sequence len is limited by numpy at 32
-
-        self.row_data = np.loadtxt('./stock_data.csv', delimiter=',', dtype=np.str)
-
-        self.data = [float(e[3]) for e in self.row_data[1:]]
-        tensor_like_data = []
-        tensor_like_deltas = []
-
-        for idx in range(0, len(self.data) - seq_len):
-            deltas = np.subtract(np.array(self.data[idx: idx + seq_len][1:]),
-                                 np.array(self.data[idx: idx + seq_len][:-1]))
-
-            # Added starting value as 0 to match sizes in G and D
-            deltas = np.concatenate([np.array([0]),
-                                     deltas])
-
-            tensor_like_deltas.append(deltas)
-            tensor_like_data.append(self.data[idx: idx + seq_len])
-
-        self.data = torch.FloatTensor(tensor_like_data)
-        self.deltas = torch.FloatTensor(tensor_like_deltas)
-
-        self.len = self.data.shape[0]
+        self.stock_data = self.stock_data[:len(self.stock_data) - 2]  # size problem
+        self.dt_data = self.dt_data[:len(self.dt_data) - 2]  # size problem
 
     def __len__(self):
-        return self.len
+        return len(self.stock_data)
 
-    def __getitem__(self, idx):
-        return self.data[idx], self.deltas[idx]
-
-
-def test_stock_dataset() -> None:
-    ds = StockDataset()
-    dl = DataLoader(ds, batch_size=5, shuffle=True, num_workers=10)
-    for i, real in enumerate(dl):
-        print('Index: {} data {}'.format(i, real))
-        break
-
-    ds = StockDataset(normalize=True)
-    dl = DataLoader(ds, batch_size=10, shuffle=True, num_workers=10)
-    for i, batch in enumerate(dl):
-        print('Index: {} data {}'.format(i, batch))
-        break
-
-
-def test_deltas_stock_dataset() -> None:
-    ds = StockDatasetDeltas()
-    dl = DataLoader(ds, batch_size=5, shuffle=False, num_workers=10)
-    for i, real in enumerate(dl):
-        data, deltas = real
-        print('Shapes: data -> {} | deltas: {}'.format(data.shape, deltas.shape))
-        print('Index: {} data {}'.format(i, deltas))
-        break
+    def __getitem__(self, idx: int):
+        return self.stock_data[idx], self.dt_data[idx]
 
 
 class SinWaveDataset(Dataset):
@@ -143,9 +81,17 @@ class SinWaveDataset(Dataset):
 
 
 if __name__ == '__main__':
-    path = './csv/sinewave.csv'
-    ds = SinWaveDataset(path, 100)
-    dl = DataLoader(ds, batch_size=10, num_workers=10, shuffle=False)
-    for idx, e in enumerate(dl):
-        d, dt = e
-        print('For current idx: {} we have x: {} and dt: {}'.format(idx, d.shape, dt.shape))
+    # path = './csv/sinewave.csv'
+    # ds = SinWaveDataset(path, 100)
+    # dl = DataLoader(ds, batch_size=10, num_workers=10, shuffle=False)
+    # for idx, e in enumerate(dl):
+    #     d, dt = e
+    #     print('For current idx: {} we have x: {} and dt: {}'.format(idx, d.shape, dt.shape))
+
+    path = './csv/stock_data.csv'
+    ds = StockDataset(path, 10, 'Close')
+    dl = DataLoader(ds, batch_size=10, shuffle=False, num_workers=10)
+    for idx, (data, dt) in enumerate(dl):
+        print(data.shape)
+        print(dt.shape)
+        break
