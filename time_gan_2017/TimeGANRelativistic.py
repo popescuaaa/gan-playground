b301a55536a14335d9c180a5319a3ead52724fa3
Popescu Andrei <andreigabrielpopescu556@gmail.com>
2021-04-25 16:49:17 +0300

    Finished general dataset acceptance version

--- time_gan_2017/TimeGANRelativistic.py
+++ time_gan_2017/TimeGANRelativistic.py
@@ -13,6 +13,7 @@ import numpy as np
 from plot import plot_time_series
 from sklearn.metrics import mean_absolute_error
 from metrics import visualization_metrics
+from datasets import GeneralDataset
 
 
 class TimeGAN:
@@ -42,6 +43,9 @@ class TimeGAN:
         self.seq_len = int(cfg['system']['seq_len'])
         self.learning_rate = float(cfg['system']['learning_rate'])
         self.num_epochs = int(cfg['system']['num_epochs'])
+        self.num_samples = int(cfg['system']['num_samples'])
+        self.model = cfg['system']['model']
+        self.dataset = cfg['system']['dataset']
 
         self.g = LSTMGenerator(self.g_dim_latent,
                                self.g_dim_output,
@@ -53,7 +57,8 @@ class TimeGAN:
                                    self.d_dim_hidden).to(self.device)
 
         # Data
-        self.ds = StockDataset('./csv/AAPL.csv', seq_len=self.seq_len, config='Close')
+        self.ds_generator = GeneralDataset.GeneralDataset(seq_len=self.seq_len, ds_type=self.dataset, model=self.model)
+        self.ds = self.ds_generator.get_dataset()
         self.dl = DataLoader(self.ds, self.batch_size, shuffle=True, num_workers=10)
 
         # Optimizer
@@ -62,13 +67,16 @@ class TimeGAN:
 
         self.criterion = nn.BCEWithLogitsLoss()
 
-    def train_discriminator(self, noise_stock, stock, dt, mean):
+    def train_discriminator(self, noise_stock, stock, dt):
         self.d_optimizer.zero_grad()
         self.d.train()
         self.d.requires_grad_(True)
         self.g.requires_grad_(False)
 
-        fake = self.g(noise_stock, dt, mean)
+        mean = stock.mean()
+        std = stock.std()
+
+        fake = self.g(noise_stock, dt, mean, std)
 
         d_real = self.d(stock, dt, mean)
         d_fake = self.d(fake, dt, mean)
@@ -79,13 +87,16 @@ class TimeGAN:
 
         return loss, fake
 
-    def train_generator(self, noise_stock, stock, dt, mean):
+    def train_generator(self, noise_stock, stock, dt):
         self.g_optimizer.zero_grad()
         self.g.train()
         self.d.requires_grad_(False)
         self.g.requires_grad_(True)
 
-        fake = self.g(noise_stock, dt, mean)
+        mean = stock.mean()
+        std = stock.std()
+
+        fake = self.g(noise_stock, dt, mean, std)
         d_fake = self.d(fake, dt, mean)
         d_real = self.d(stock, dt, mean)
 
@@ -103,20 +114,21 @@ class TimeGAN:
         generated_distribution = self.g(_noise,
                                         self.ds[0][1].float().unsqueeze(0)
                                         .unsqueeze(2).to(self.device),  # dt
-                                        self.ds[0][0].float().mean().to(self.device)) \
+                                        self.ds[0][0].float().mean().to(self.device),
+                                        self.ds[0][0].float().std().to(self.device)) \
             .detach().cpu().view(1, -1).numpy()
 
         # Generate n samples with current form of the g
-        for _stock, _dt in self.ds[1:]:
-            _stock = _stock.float()
+        for _real, _dt in self.ds[1:]:
+            _real = _real.float()
             _dt = _dt.float()
 
             generated_distribution = np.append(generated_distribution, self.g(_noise,
                                                                               _dt.unsqueeze(0)
                                                                               .unsqueeze(2).to(self.device),
                                                                               # dt
-                                                                              _stock.mean().to(
-                                                                                  self.device))  # mean
+                                                                              _real.mean().to(self.device),
+                                                                              _real.std().to(self.device))  # mean
                                                .detach().cpu().view(1, -1).numpy(), axis=0)
 
         _generated_distribution = np.array(list(map(self.ds.mean_reshape, generated_distribution)))
@@ -127,29 +139,27 @@ class TimeGAN:
     def train_system(self):
         for epoch in range(self.num_epochs):
             for batch_idx, real in enumerate(self.dl):
-                stock, dt = real
+                real_data, dt = real
 
-                stock = stock.view(*stock.shape, 1)
-                stock = stock.float()
-                stock = stock.to(self.device)
-
-                mean = stock.mean()
+                real_data = real_data.view(*real_data.shape, 1)
+                real_data = real_data.float()
+                real_data = real_data.to(self.device)
 
                 dt = dt.view(*dt.shape, 1)
                 dt = dt.float()
                 dt = dt.to(self.device)
 
-                noise_stock = self.dist_latent.sample(sample_shape=(self.batch_size, self.seq_len, self.g_dim_latent))
-                noise_stock = noise_stock.to(self.device)
+                noise = self.dist_latent.sample(sample_shape=(self.batch_size, self.seq_len, self.g_dim_latent))
+                noise = noise.to(self.device)
 
                 g_iter = self.g_iter
                 d_iter = self.d_iter
 
                 for var_name in range(g_iter):
-                    loss_g, _ = self.train_generator(noise_stock, stock, dt, mean)
+                    loss_g, _ = self.train_generator(noise, real_data, dt)
 
                 for var_name in range(d_iter):
-                    loss_d, fake = self.train_discriminator(noise_stock, stock, dt, mean)
+                    loss_d, fake = self.train_discriminator(noise, real_data, dt)
 
                 if batch_idx == 0:
                     print(
@@ -165,13 +175,13 @@ class TimeGAN:
                         'epoch': epoch,
                         'd loss': loss_d.detach().cpu().item(),
                         'g loss': loss_g.detach().cpu().item(),
-                        'mae': mean_absolute_error(stock[0].view(-1).cpu().numpy(),
+                        'mae': mean_absolute_error(real_data[0].view(-1).cpu().numpy(),
                                                    fake[0].view(-1).detach().cpu().numpy()),
                         'Conditional on deltas fake sample': plot_time_series(
                             fake[0].view(-1).detach().cpu().numpy(),
                             '[Conditional (on deltas)] Fake sample'),
                         'Real sample': plot_time_series(
-                            stock[0].view(-1).cpu().numpy(),
+                            real_data[0].view(-1).cpu().numpy(),
                             '[Corresponding] Real sample'),
                         'Distribution': fig
                     })
@@ -184,19 +194,18 @@ if __name__ == '__main__':
     with open('config.yaml', 'r') as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
 
-    # run_name = str('RCGAN: {} {} \n'.format(config['run_name'], datetime.datetime.now()) + str(config.values()))
-    run_name = '1g / 16d | G mean condition'
+    run_name = config['system']['run_name'] + ' ' + config['system']['dataset']
 
     wandb.init(config=config, project='time-gan-2017', name=run_name)
 
     time_gan = TimeGAN(config)
     time_gan.train_system()
 
-    # time_gan.g = time_gan.g.to('cpu')
-    # time_gan.d = time_gan.d.to('cpu')
-    #
-    # torch.save(time_gan.g.state_dict(), './models/time_gan_2017_g.pt')
-    # torch.save(time_gan.d.state_dict(), './models/time_gan_2017_d.pt')
-    #
-    # time_gan.g = time_gan.g.to(config['system']['device'])
-    # time_gan.d = time_gan.d.to(config['system']['device'])
+    time_gan.g = time_gan.g.to('cpu')
+    time_gan.d = time_gan.d.to('cpu')
+
+    torch.save(time_gan.g.state_dict(), './trained_models/rcgan_g.pt')
+    torch.save(time_gan.d.state_dict(), './trained_models/rcgan_d.pt')
+
+    time_gan.g = time_gan.g.to(config['system']['device'])
+    time_gan.d = time_gan.d.to(config['system']['device'])
